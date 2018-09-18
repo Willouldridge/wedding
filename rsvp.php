8c2294dec4db64b907af5b99c581bef41fde26e9
Will Ouldridge <william.ouldridge@hotmail.com>
2018-09-18 21:54:30 +0100

    SQL test

--- rsvp.php
+++ rsvp.php
@@ -4,12 +4,41 @@ if(!isset($_POST['submit']))
 	//This page should not be accessed directly. Need to submit the form.
 	echo "error; you need to submit the form!";
 }
-  $first_name = $_POST['firstname'];
-  $last_name = $_POST['lastname'];
-  $attending = $_POST['attending'];
-  $vegetarian = $_POST['vegetarian'];
-  $vegan = $_POST['vegan'];
-  $gluten_free = $_POST['gluten_free'];
-  $other_reqs = $_POST['other_reqs'];
+  //$first_name = $_POST['firstname'];
+  //$last_name = $_POST['lastname'];
+  //$attending = $_POST['attending'];
+  //$vegetarian = $_POST['vegetarian'];
+  //$vegan = $_POST['vegan'];
+  //$gluten_free = $_POST['gluten_free'];
+  //$other_reqs = $_POST['other_reqs'];
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
 
 ?>
